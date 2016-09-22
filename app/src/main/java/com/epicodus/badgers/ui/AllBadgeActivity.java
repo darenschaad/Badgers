1a9c81a0985094a00ba1cece7083e3f3497485ab
Daren Schaad <darenschaad@gmail.com>
2016-09-22 13:02:41 -0700

    Created SearchBadgeActivity and AllBadgeActivity

--- app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
+++ app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
@@ -1,13 +1,15 @@
-package com.epicodus.badgers;
+package com.epicodus.badgers.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.epicodus.badgers.R;
+
+public class AllBadgeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_all_badge);
     }
 }
