31b2b419321b1a6164d370d113a0c40cd2c383e4
Daren Schaad <darenschaad@gmail.com>
2016-09-23 12:19:52 -0700

    imported ButterKnife and set up intent to SearchActivity

--- app/src/main/java/com/epicodus/badgers/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/badgers/ui/MainActivity.java
@@ -1,15 +1,37 @@
 package com.epicodus.badgers.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.epicodus.badgers.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.searchEditText) EditText mSearchEditText;
+    @BindView(R.id.searchButton) Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.searchButton:
+                String keyword = mSearchEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SearchBadgeActivity.class);
+                intent.putExtra("keyword",keyword);
+                startActivity(intent);
+        }
     }
 }
