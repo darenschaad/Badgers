e254f61a5bcc6bbeca9d01e3ce035af8cf50ebab
Daren Schaad <darenschaad@gmail.com>
2016-09-23 13:09:47 -0700

    set onClickListeners for search and view all buttons on MainActivity

--- app/src/main/java/com/epicodus/badgers/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/badgers/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.epicodus.badgers.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,22 +17,31 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.searchEditText) EditText mSearchEditText;
     @BindView(R.id.searchButton) Button mSearchButton;
+    @BindView(R.id.viewAllButton) Button mViewAllButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mSearchButton.setOnClickListener(this);
+        mViewAllButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.searchButton:
+                Log.d("button", "button Clicked");
                 String keyword = mSearchEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SearchBadgeActivity.class);
                 intent.putExtra("keyword",keyword);
                 startActivity(intent);
+                break;
+            case R.id.viewAllButton:
+                Intent intent1 = new Intent(MainActivity.this, AllBadgeActivity.class);
+                startActivity(intent1);
+                break;
         }
     }
 }
