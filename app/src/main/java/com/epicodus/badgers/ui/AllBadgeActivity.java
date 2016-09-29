3a48f5ce3a49393400b4b65bb276533d606df50d
Daren Schaad <darenschaad@gmail.com>
2016-09-29 14:58:14 -0700

    added more dependencies for firebase

--- app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
+++ app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
@@ -4,8 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.badgers.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AllBadgeActivity extends AppCompatActivity {
+
+    private DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
