447877a8e9456fc1d1c51be25f064a58b967ac80
Daren Schaad <darenschaad@gmail.com>
2016-09-29 15:15:32 -0700

    made contact with firebase through AllBadgesActivity

---
+++ app/src/main/java/com/epicodus/badgers/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.badgers;
+
+/**
+ * Created by Daren on 9/29/2016.
+ */
+
+public class Constants {
+    public static final String FIREBASE_LOCATION_BADGES = "badges";
+}
--- app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
+++ app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
@@ -2,18 +2,40 @@ package com.epicodus.badgers.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.epicodus.badgers.Constants;
 import com.epicodus.badgers.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AllBadgeActivity extends AppCompatActivity {
 
-    private DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_badge);
+
+        mRef  = FirebaseDatabase.getInstance().getReference().child(Constants.FIREBASE_LOCATION_BADGES);
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot badgeSnapShot : dataSnapshot.getChildren()) {
+                    String name = badgeSnapShot.getValue().toString();
+                    Log.d("FireBase Test", name);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
