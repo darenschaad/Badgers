98c004d50215e3403d7cc01001ec0a4685fe6727
Daren Schaad <darenschaad@gmail.com>
2016-10-03 12:55:04 -0700

    added detail fragment and layout

---
+++ app/src/main/java/com/epicodus/badgers/ui/BadgeDetailActivity.java
@@ -0,0 +1,14 @@
+package com.epicodus.badgers.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.epicodus.badgers.R;
+
+public class BadgeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_badge_detail);
+    }
+}
