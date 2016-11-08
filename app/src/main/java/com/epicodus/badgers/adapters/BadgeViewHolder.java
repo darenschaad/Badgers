4210a3055974594bc58d8d9525840073f511238c
Daren Schaad <darenschaad@gmail.com>
2016-11-08 11:32:10 -0800

    Programmatically set colors in listview for view all badges

--- app/src/main/java/com/epicodus/badgers/adapters/BadgeViewHolder.java
+++ app/src/main/java/com/epicodus/badgers/adapters/BadgeViewHolder.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.epicodus.badgers.R;
@@ -19,6 +21,7 @@ import org.parceler.Parcels;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,12 +35,17 @@ public class BadgeViewHolder extends RecyclerView.ViewHolder implements View.OnC
     @BindView(R.id.badgeNameTextView) TextView mNameTextView;
 //    @BindView(R.id.categoryTextView) TextView mCategoryTextView;
     @BindView(R.id.tagTextView) TextView mTagTextView;
-
+    @BindView(R.id.listBackground) LinearLayout mListBackground;
 
     private ArrayList<Badge> mBadges = new ArrayList<>();
     private Context mContext;
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
+
+    Integer[] categories = {0,100,200,300,400,500,600,700,800,900};
+    int[] categoryTextColors = {R.color.category0Text, R.color.category100Text,R.color.category200Text, R.color.category300Text, R.color.category400Text, R.color.category500Text, R.color.category600Text, R.color.category700Text, R.color.category800Text, R.color.category900Text};
+    int[] categoryColors = {R.color.category0Primary, R.color.category100Primary, R.color.category200Primary, R.color.category300Primary, R.color.category400Primary, R.color.category500Primary, R.color.category600Primary, R.color.category700Primary, R.color.category800Primary, R.color.category900Primary};
+//    String[] categoryText = {"000 - GENERAL KNOWLEDGE", "100 - PHILOSOPHY & PSYCHOLOGY", "200 - RELIGION", "300 - SOCIAL SCIENCE", "400 - LANGUAGES", "500 - SCIENCE", "600 - TECHNOLOGY", "700 - ARTS & RECREATION", "800 - LITERATURE", "900 - HISTORY & GEOGRAPHY"};
 
     public BadgeViewHolder(ArrayList<Badge> badges, View itemView) {
         super(itemView);
@@ -57,22 +65,24 @@ public class BadgeViewHolder extends RecyclerView.ViewHolder implements View.OnC
 //            }
 //        } else {
 //            Picasso.with(itemView.getContext()).load(badge.getImageUrl()).resize(MAX_WIDTH,MAX_HEIGHT).centerCrop().into(mBadgeImageView);
-//
 //        }
         mNameTextView.setText(badge.getName());
-
 //        mCategoryTextView.setText(String.valueOf(badge.getCategory()));
 //        mCategoryTextView.setRotation(-90);
 //        mTagTextView.setText(badge.getTags().get(0));
-
+        int category = badge.getCategory();
+        int categoryIndex = Arrays.asList(categories).indexOf(category);
+        int categoryTextColor = categoryTextColors[categoryIndex];
+        int categoryColor = categoryColors[categoryIndex];
+        mNameTextView.setTextColor(ContextCompat.getColor(mContext, categoryTextColor));
+        mListBackground.setBackgroundColor(ContextCompat.getColor(mContext,categoryColor));
     }
+
 
     public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
         byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
         return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
     }
-
-
 
     @Override
     public void onClick(View v) {
