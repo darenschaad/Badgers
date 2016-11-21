cb38147c32982a003ce172d7ba726546a9b8ec18
Daren Schaad <darenschaad@gmail.com>
2016-11-21 15:42:55 -0800

    filter spinner is functioning, and categories page is working too

--- app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
+++ app/src/main/java/com/epicodus/badgers/ui/AllBadgeActivity.java
@@ -1,10 +1,13 @@
 package com.epicodus.badgers.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -28,42 +31,122 @@ public class AllBadgeActivity extends AppCompatActivity {
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     @BindView(R.id.filterSpinner) Spinner mFilterSpinner;
     private BadgeListAdapter mAdapter;
-
-
     private DatabaseReference mRef;
     private ValueEventListener mRefListener;
     public ArrayList<Badge> mBadges = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_badge);
         ButterKnife.bind(this);
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category");
+
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
                 R.array.filterArray, R.layout.spinner_item);
         mFilterSpinner.setAdapter(adapter);
+        if (category != null){
+            switch (category) {
+                case "000":
+                    mFilterSpinner.setSelection(1);
+                    break;
+                case "100":
+                    mFilterSpinner.setSelection(2);
+                    break;
+            }
 
-        mRef  = FirebaseDatabase.getInstance().getReference().child(Constants.FIREBASE_LOCATION_BADGES);
+        }
 
-        mRefListener = mRef.addValueEventListener(new ValueEventListener() {
+        mFilterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot badgeSnapShot : dataSnapshot.getChildren()) {
-                    Badge badge = badgeSnapShot.getValue(Badge.class);
-                    mBadges.add(badge);
-                }
-                mAdapter = new BadgeListAdapter(getApplicationContext(), mBadges);
-                mRecyclerView.setAdapter(mAdapter);
-                RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(AllBadgeActivity.this);
-                mRecyclerView.setLayoutManager(layoutManager);
-                mRecyclerView.setHasFixedSize(true);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                mBadges.clear();
+                final String filterString = Integer.toString(mFilterSpinner.getSelectedItemPosition());
+                mRef  = FirebaseDatabase.getInstance().getReference().child(Constants.FIREBASE_LOCATION_BADGES);
+                mRefListener = mRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot badgeSnapShot : dataSnapshot.getChildren()) {
+                            Badge badge = badgeSnapShot.getValue(Badge.class);
+                            Log.d("Filter", filterString);
+                            switch (filterString){
+                                case "0":
+                                    mBadges.add(badge);
+                                    break;
+                                case "1":
+                                    if(badge.getCategory() == 0) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "2":
+                                    if(badge.getCategory() == 100) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "3":
+                                    if(badge.getCategory() == 200) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "4":
+                                    if(badge.getCategory() == 300) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "5":
+                                    if(badge.getCategory() == 400) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "6":
+                                    if(badge.getCategory() == 500) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "7":
+                                    if(badge.getCategory() == 600) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "8":
+                                    if(badge.getCategory() == 700) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "9":
+                                    if(badge.getCategory() == 800) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                                case "10":
+                                    if(badge.getCategory() == 900) {
+                                        mBadges.add(badge);
+                                    }
+                                    break;
+                            }
+
+                        }
+                        mAdapter = new BadgeListAdapter(getApplicationContext(), mBadges);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(AllBadgeActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
     }
--- app/src/main/java/com/epicodus/badgers/ui/CategoryActivity.java
+++ app/src/main/java/com/epicodus/badgers/ui/CategoryActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.epicodus.badgers.R;
 
@@ -12,24 +14,45 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class CategoryActivity extends AppCompatActivity implements View.OnClickListener{
-    @BindView(R.id.cat000Layout) RelativeLayout mCat000Layout;
-    @BindView(R.id.cat100Layout) RelativeLayout mCat100Layout;
+    @BindView(R.id.cat000TextView) TextView mCat000TextView;
+    @BindView(R.id.cat000ImageView) ImageView mCat000ImageView;
+    @BindView(R.id.cat100TextView) TextView mCat100TextView;
+    @BindView(R.id.cat100ImageView) ImageView mCat100ImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         ButterKnife.bind(this);
-        mCat000Layout.setOnClickListener(this);
-        mCat100Layout.setOnClickListener(this);
+        mCat000TextView.setOnClickListener(this);
+        mCat000ImageView.setOnClickListener(this);
+        mCat100TextView.setOnClickListener(this);
+        mCat000ImageView.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.cat000Layout:
+            case R.id.cat000TextView:
                 Intent intent = new Intent(CategoryActivity.this, AllBadgeActivity.class);
+                intent.putExtra("category", "000");
                 startActivity(intent);
+                break;
+            case R.id.cat000ImageView:
+                Intent intent2 = new Intent(CategoryActivity.this, AllBadgeActivity.class);
+                intent2.putExtra("category", "000");
+                startActivity(intent2);
+                break;
+            case R.id.cat100TextView:
+                Intent intent3 = new Intent(CategoryActivity.this, AllBadgeActivity.class);
+                intent3.putExtra("category", "100");
+                startActivity(intent3);
+                break;
+            case R.id.cat100ImageView:
+                Intent intent4 = new Intent(CategoryActivity.this, AllBadgeActivity.class);
+                intent4.putExtra("category", "100");
+                startActivity(intent4);
                 break;
         }
     }
